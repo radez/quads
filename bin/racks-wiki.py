433dd164626c1fdda2396d9eba4e8ffc9422baa6
Gonzalo Rafuls <grafuls@redhat.com>
2018-09-05 18:42:57 +0200

    Ported over to python3

--- bin/racks-wiki.py
+++ bin/racks-wiki.py
@@ -9,19 +9,23 @@ from wordpress_xmlrpc.methods.taxonomies import *
 from wordpress_xmlrpc.methods.posts import *
 from wordpress_xmlrpc.methods.users import *
 from wordpress_xmlrpc.methods import *
-from subprocess import call
 import argparse
-import os
 
 parser = argparse.ArgumentParser(description='Generate WordPress WIKI page from Markdown')
 parser.add_argument('--markdown', dest='markdown', type=str, default=None, help='Specify markdown input file')
 parser.add_argument('--page-id', dest='pageid', type=int, default=4, help='Specify wordpress page id. default = 4')
-parser.add_argument('--wp-url', dest='wpurl', type=str, default=None, help='Specify wordpress URL. e.g. http://wiki.example.com/xmlrpc.php')
+parser.add_argument(
+    '--wp-url',
+    dest='wpurl',
+    type=str,
+    default=None,
+    help='Specify wordpress URL. e.g. http://wiki.example.com/xmlrpc.php'
+)
 parser.add_argument('--wp-username', dest='wpusername', type=str, default=None, help='Specify wordpress username.')
 parser.add_argument('--wp-password', dest='wppassword', type=str, default=None, help='Specify wordpress password.')
 parser.add_argument('--page-title', dest='pagetitle', type=str, default=None, help='Specify the wiki post title.')
 
-args=parser.parse_args()
+args = parser.parse_args()
 
 # authentication and wp url
 wp_url = args.wpurl
@@ -30,9 +34,11 @@ wp_password = args.wppassword
 markdown = args.markdown
 pagetitle = args.pagetitle
 
+
 def missing_arg(parameter):
-    print "Required parameter missing: " + parameter
+    print("Required parameter missing: " + parameter)
     exit(1)
+
 
 if wp_url is None:
     missing_arg('--wp-url')
