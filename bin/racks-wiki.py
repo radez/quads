37305aa5b636e456aa06c90b8c48e2f196ec0548
Gonzalo Rafuls <grafuls@gmail.com>
2018-10-05 15:47:14 +0200

    First steps to integrate regenerate-wiki script to Quads.
    Added simple Foreman API wrapper to avoid calling hammer.
    TODO: Deal with Foreman credentials.
    
    Change-Id: I60c0a89c2a10141bbd815f7d800e422140dbe947

--- bin/racks-wiki.py
+++ bin/racks-wiki.py
@@ -5,68 +5,80 @@
 # requires: python-wordpress-xmlrpc or python3-wordpress-xmlrpc
 
 from wordpress_xmlrpc import *
-from wordpress_xmlrpc.methods.taxonomies import *
 from wordpress_xmlrpc.methods.posts import *
 from wordpress_xmlrpc.methods.users import *
-from wordpress_xmlrpc.methods import *
 import argparse
 
-parser = argparse.ArgumentParser(description='Generate WordPress WIKI page from Markdown')
-parser.add_argument('--markdown', dest='markdown', type=str, default=None, help='Specify markdown input file')
-parser.add_argument('--page-id', dest='pageid', type=int, default=4, help='Specify wordpress page id. default = 4')
-parser.add_argument(
-    '--wp-url',
-    dest='wpurl',
-    type=str,
-    default=None,
-    help='Specify wordpress URL. e.g. http://wiki.example.com/xmlrpc.php'
-)
-parser.add_argument('--wp-username', dest='wpusername', type=str, default=None, help='Specify wordpress username.')
-parser.add_argument('--wp-password', dest='wppassword', type=str, default=None, help='Specify wordpress password.')
-parser.add_argument('--page-title', dest='pagetitle', type=str, default=None, help='Specify the wiki post title.')
 
-args = parser.parse_args()
+def update_wiki(url, username, password, _page_title, _page_id, _markdown):
+    wp = Client(url, username, password)
 
-# authentication and wp url
-wp_url = args.wpurl
-wp_username = args.wpusername
-wp_password = args.wppassword
-markdown = args.markdown
-pagetitle = args.pagetitle
+    # define pages variable
+    page = WordPressPage()
+    page.title = _page_title
+
+    # page id can be found by viewing via wp-admin dashboard in URL
+    page.id = _page_id
+
+    # set local content file to read handle info into a string
+    with open(_markdown, 'r') as _file:
+        page.content = _file.read()
+
+    # post new content to the page
+    wp.call(EditPost(page.id, page))
 
 
-def missing_arg(parameter):
-    print("Required parameter missing: " + parameter)
-    exit(1)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate WordPress WIKI page from Markdown')
+    parser.add_argument(
+        '--markdown',
+        dest='markdown',
+        type=str,
+        default=None,
+        help='Specify markdown input file',
+        required=True)
+    parser.add_argument(
+        '--page-id',
+        dest='pageid',
+        type=int,
+        default=4,
+        help='Specify wordpress page id. default = 4')
+    parser.add_argument(
+        '--wp-url',
+        dest='wpurl',
+        type=str,
+        default=None,
+        help='Specify wordpress URL. e.g. http://wiki.example.com/xmlrpc.php',
+        required=True)
+    parser.add_argument(
+        '--wp-username',
+        dest='wpusername',
+        type=str,
+        default=None,
+        help='Specify wordpress username.',
+        required=True)
+    parser.add_argument(
+        '--wp-password',
+        dest='wppassword',
+        type=str,
+        default=None,
+        help='Specify wordpress password.',
+        required=True)
+    parser.add_argument(
+        '--page-title',
+        dest='pagetitle',
+        type=str,
+        default='Example Wiki Page',
+        help='Specify the wiki post title.')
 
+    args = parser.parse_args()
 
-if wp_url is None:
-    missing_arg('--wp-url')
+    # authentication and wp url
+    wp_url = args.wpurl
+    wp_username = args.wpusername
+    wp_password = args.wppassword
+    markdown = args.markdown
+    page_title = args.pagetitle
+    page_id = args.pageid
 
-if wp_username is None:
-    missing_arg('--wp-username')
-
-if wp_password is None:
-    missing_arg('--wp-password')
-
-if markdown is None:
-    missing_arg('--markdown')
-
-if pagetitle is None:
-    pagetitle = 'Example Wiki Page'
-
-wp = Client(wp_url, wp_username, wp_password)
-
-# define pages variable
-page = WordPressPage()
-page.title = pagetitle
-
-# page id can be found by viewing via wp-admin dashboard in URL
-page.id = args.pageid
-
-# set local content file to read handle info into a string
-f = open(markdown, 'r')
-page.content = f.read()
-
-# post new content to the page
-wp.call(EditPost(page.id, page))
+    update_wiki(wp_url, wp_username, wp_password, page_title, page_id)
